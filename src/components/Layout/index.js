b4d24a26746121383d6f1d57b8fa01217a2a58f5
songminseok <ske3056@gmail.com>
2023-07-14 16:46:21 +0900

    Seo

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,10 @@
 import NavBar from "../NavBar";
+import Seo from "../Seo";
 
 export default function Layout() {
   return (
     <>
+      <Seo></Seo>
       <NavBar />
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
+import Seo from "@/components/Seo";
+
 export default function Home() {
   return (
     <>
+      <Seo title="home" />
+
       <h1>hello world</h1>
     </>
   );
